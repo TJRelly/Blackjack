2a2964a2548e2b16196ba9042e459d1b9499ce32
TJ Jackson <94482664+TJRelly@users.noreply.github.com>
2022-12-17 20:02:25 +0000

    added shuffle and deal function

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+
+cards = []
+
+suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
+
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+for suit in suits:
+    for rank in ranks:
+        cards.append([suit, rank])
+
+def shuffle():
+    random.shuffle(cards)
+
+def deal(num):
+    cards_dealt = []
+    for x in range(num):
+        card = cards.pop()
+        cards_dealt.append(card)
+    return cards_dealt
+
+shuffle()
+cards_dealt = deal(2)
+card = cards_dealt[0]
+rank = card[1]
+
+if rank == "A":
+    value = 11
+elif rank == "J" or rank == "Q" or rank == "K":
+    value = 10
+else:
+    value = rank
+
+rank_dict = {"rank": rank, "value": value}
+
+print(rank_dict["rank"], rank_dict["value"])
+
+        
+
